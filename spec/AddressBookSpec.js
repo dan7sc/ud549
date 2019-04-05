ac939d99bce69aab9a1a29dcf355a83971e1228a
daniel sc <su7cheng@gmail.com>
2019-04-05 15:09:59 -0300

    Create Contact() function.

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -2,7 +2,7 @@ describe('Address Book', function() {
     var addressBook = new AddressBook(),
         thisContact = new Contact();
 
-    if('should be able to add a contact', function() {
+    it('should be able to add a contact', function() {
         addressBook.addContact(thisContact);
 
         expect(addressBook.getContact(0)).toBe(thisContact);
